63a712e85141dd101f74ebf77ab4f0b1b9f29557
CH SHAKISH <128886167+CHSHAKISH@users.noreply.github.com>
2026-05-29 01:13:34 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string str = "";
+
+        for(char ch : s){
+            if(isalnum(ch)) str += tolower(ch);
+        }
+
+        string revStr = str;
+
+        // reverse(revStr.begin(), revStr.end());
+
+        // return str == revStr;
+
+        return str == string(str.rbegin(), str.rend());
+        
+    }
+};
